b1b9e1d139695dd542446d65c534f70c3e175116
NaveenReddy0427 <146743492+NaveenReddy0427@users.noreply.github.com>
2025-09-01 20:36:21 +0530

    Update4 app.js

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get("/", (req, res) => {
 });
 
 
-app.listen(PORT, () => {
-  console.log(`Server listening at PORT ${PORT}.`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server listening at PORT ${PORT}`);
 });
